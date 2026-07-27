3b7daf2c9a0ca76a5306691f5bbca6248addcbd9
jaimef7 <jaimef@junior.localdomain>
2026-07-27 17:37:02 -0500

    added display for update and clear

--- functions.js
+++ functions.js
@@ -11,9 +11,9 @@ function divide(a,b){
     return a / b;
 }
 
-let displaynumber1;
-let operand;
-let displaynumber2;
+let displaynumber1 = "";
+let operand = ""
+let displaynumber2 = "";
 
 function operate (firstnumber, secondnumber,operand) {
     switch(operand){
@@ -30,3 +30,44 @@ function operate (firstnumber, secondnumber,operand) {
     }
     
 }
+
+const container = document.getElementById("keys")
+const display = document.getElementById("display")
+
+function updatedisplay(value){
+        display.textContent =value.toString()
+
+    }
+
+    function cleardisplay(){
+        display.textContent = "";
+        displaynumber1.textContent = "";
+        displaynumber2.textContent = "";
+
+    }
+
+container.addEventListener('click', (event) => {
+    let value =  event.target.textContent;
+
+    if (value === '+' || value === '-' || value === '*' || value === '/'){
+        operand = value; 
+    }else if(value == '='){
+        const result = operate(Number(displaynumber1), Number(displaynumber2), operand);
+        updatedisplay(result);
+
+        displaynumber1 = result.toString();
+        displaynumber2 = "";
+        operand = "";
+    }else if (value == 'C'){
+        cleardisplay()
+    }else{
+        if(operand !==""){
+            displaynumber2 += value;
+            updatedisplay(displaynumber2)
+        }else{
+            displaynumber1 += value
+            updatedisplay(displaynumber1)
+        }
+    }
+})
+
